2259e1011e4cec7b64993d64697c131f10f9a47b
ishihara.jp <ishihara.jp@gmail.com>
2016-03-14 11:24:56 +0900

    Add

--- javascripts/Senario.js
+++ javascripts/Senario.js
@@ -4,13 +4,15 @@ function senarioDemo(){
     var SecPerYear = ANIMATION_TIME/35 + 350*2;
     var HighlightTime = SecPerYear * 0.3;
     
+    var _senario = svg.select("senario");
+    
     //1982
     setTimeout(function(){
         filterByCompany("NEC");
-        senario.text("NEC：PC-9800シリーズ発売");
+        _senario.text("NEC：PC-9800シリーズ発売");
 
         setTimeout(function(){
-            senario.text("");
+            _senario.text("");
             filterReset();
             setDisplayAll();
         }, HighlightTime);
@@ -20,9 +22,9 @@ function senarioDemo(){
     //1983
     setTimeout(function(){
         filterByCompany("CASIO");
-        senario.text("カシオ：腕時計「G-SHOCK」発売");
+        _senario.text("カシオ：腕時計「G-SHOCK」発売");
         setTimeout(function(){
-            senario.text("");
+            _senario.text("");
             filterReset();
             setDisplayAll();
         }, HighlightTime);
@@ -31,9 +33,9 @@ function senarioDemo(){
     //1984
     setTimeout(function(){
         filterByCompany("KYOCERA");
-        senario.text("京セラ：第二電電（DDI）設立。後にKDD、IDOと合併し、KDDIとなる");
+        _senario.text("京セラ：第二電電（DDI）設立。後にKDD、IDOと合併し、KDDIとなる");
         setTimeout(function(){
-            senario.text("");
+            _senario.text("");
             filterReset();
             setDisplayAll();
         }, HighlightTime);
@@ -43,9 +45,9 @@ function senarioDemo(){
     setTimeout(function(){
         filterByCompany("TOSHIBA");
         filterByCompany("INTEL");
-        senario.text("インテル：DRAM事業撤退。CPUの開発・生産に経営資源を集中　/　東芝：世界初1メガDRAM開発。メモリ開発分野で世界トップへ");
+        _senario.text("インテル：DRAM事業撤退。CPUの開発・生産に経営資源を集中　/　東芝：世界初1メガDRAM開発。メモリ開発分野で世界トップへ");
         setTimeout(function(){
-            senario.text("");
+            _senario.text("");
             filterReset();
             setDisplayAll();
         }, HighlightTime*1.5);
@@ -54,9 +56,9 @@ function senarioDemo(){
     //1986
     setTimeout(function(){
         filterByCompany("FUJIFILM");
-        senario.text("富士フィルム：レンズ付きフィルム「写ルンです」発売");
+        _senario.text("富士フィルム：レンズ付きフィルム「写ルンです」発売");
         setTimeout(function(){
-            senario.text("");
+            _senario.text("");
             filterReset();
             setDisplayAll();
         }, HighlightTime);
@@ -65,9 +67,9 @@ function senarioDemo(){
     //1987
     setTimeout(function(){
         filterByCompany("NEC");
-        senario.text("NEC：家庭用ゲーム機「PCエンジン」発売");
+        _senario.text("NEC：家庭用ゲーム機「PCエンジン」発売");
         setTimeout(function(){
-            senario.text("");
+            _senario.text("");
             filterReset();
             setDisplayAll();
         }, HighlightTime);
@@ -76,9 +78,9 @@ function senarioDemo(){
     //1988
     setTimeout(function(){
         filterByCompany("PANASONIC");
-        senario.text("パナソニック：家電を「National」ブランドから「Panasonic」ブランドへ移行");
+        _senario.text("パナソニック：家電を「National」ブランドから「Panasonic」ブランドへ移行");
         setTimeout(function(){
-            senario.text("");
+            _senario.text("");
             filterReset();
             setDisplayAll();
         }, HighlightTime);
@@ -87,9 +89,9 @@ function senarioDemo(){
     //1989
     setTimeout(function(){
         filterByCompany("SONY");
-        senario.text("ソニー：コロンビア・ピクチャーズ・エンタテイメントを買収し映画事業に参入");
+        _senario.text("ソニー：コロンビア・ピクチャーズ・エンタテイメントを買収し映画事業に参入");
         setTimeout(function(){
-            senario.text("");
+            _senario.text("");
             filterReset();
             setDisplayAll();
         }, HighlightTime);
@@ -98,9 +100,9 @@ function senarioDemo(){
     //1990
     setTimeout(function(){
         filterByCompany("PIONEER");
-        senario.text("パイオニア：世界初のGPSカーナビゲーション発売");
+        _senario.text("パイオニア：世界初のGPSカーナビゲーション発売");
         setTimeout(function(){
-            senario.text("");
+            _senario.text("");
             filterReset();
             setDisplayAll();
         }, HighlightTime);
@@ -109,9 +111,9 @@ function senarioDemo(){
     //1991
     setTimeout(function(){
         filterByCompany("IBM");
-        senario.text("IBM：メインフレームの業績悪化により49億ドルの損失発表。当時、米国史上最悪値を記録");
+        _senario.text("IBM：メインフレームの業績悪化により49億ドルの損失発表。当時、米国史上最悪値を記録");
         setTimeout(function(){
-            senario.text("");
+            _senario.text("");
             filterReset();
             setDisplayAll();
         }, HighlightTime);
@@ -119,15 +121,15 @@ function senarioDemo(){
 
     //1992
     setTimeout(function(){
-        senario.text("日本ではバブル景気崩壊");
+        _senario.text("日本ではバブル景気崩壊");
     },SecPerYear*11);
 
     //1993
     setTimeout(function(){
         filterByCompany("INTEL");
-        senario.text("インテル：x86向け第5世代CPU「Pentium」を発売");
+        _senario.text("インテル：x86向け第5世代CPU「Pentium」を発売");
         setTimeout(function(){
-            senario.text("");
+            _senario.text("");
             filterReset();
             setDisplayAll();
         }, HighlightTime);
@@ -136,9 +138,9 @@ function senarioDemo(){
     //1994
     setTimeout(function(){
         filterByCompany("SONY");
-        senario.text("ソニー：家庭用ゲーム機「PlayStation」発売");
+        _senario.text("ソニー：家庭用ゲーム機「PlayStation」発売");
         setTimeout(function(){
-            senario.text("");
+            _senario.text("");
             filterReset();
             setDisplayAll();
         }, HighlightTime);
@@ -147,9 +149,9 @@ function senarioDemo(){
     //1995
     setTimeout(function(){
         filterByCompany("MICROSOFT");
-        senario.text("マイクロソフト：Windows 95 発売");
+        _senario.text("マイクロソフト：Windows 95 発売");
         setTimeout(function(){
-            senario.text("");
+            _senario.text("");
             filterReset();
             setDisplayAll();
         }, HighlightTime);
@@ -158,9 +160,9 @@ function senarioDemo(){
     //1996
     setTimeout(function(){
         filterByCompany("ERICSSON");
-        senario.text("エリクソン：ソニーと合弁でソニー・エリクソン設立。携帯事業へ進出");
+        _senario.text("エリクソン：ソニーと合弁でソニー・エリクソン設立。携帯事業へ進出");
         setTimeout(function(){
-            senario.text("");
+            _senario.text("");
             filterReset();
             setDisplayAll();
         }, HighlightTime);
@@ -169,9 +171,9 @@ function senarioDemo(){
     //1997
     setTimeout(function(){
         filterByCompany("APPLE");
-        senario.text("アップル：スティーブ・ジョブズ復帰。iMac発表やロゴデザイン一新など新生Appleを印象付ける");
+        _senario.text("アップル：スティーブ・ジョブズ復帰。iMac発表やロゴデザイン一新など新生Appleを印象付ける");
         setTimeout(function(){
-            senario.text("");
+            _senario.text("");
             filterReset();
             setDisplayAll();
         }, HighlightTime);
@@ -180,9 +182,9 @@ function senarioDemo(){
     //1998
     setTimeout(function(){
         filterByCompany("NOKIA");
-        senario.text("ノキア：エリクソン等と共同でシンビアン社設立。同社OSは日本のフューチャーフォンに多く採用。13年間にわたり携帯シェア世界トップに君臨");
+        _senario.text("ノキア：エリクソン等と共同でシンビアン社設立。同社OSは日本のフューチャーフォンに多く採用。13年間にわたり携帯シェア世界トップに君臨");
         setTimeout(function(){
-            senario.text("");
+            _senario.text("");
             filterReset();
             setDisplayAll();
         }, HighlightTime*1.5);
@@ -191,9 +193,9 @@ function senarioDemo(){
     //1999
     setTimeout(function(){
         filterByCompany("MICROSOFT");
-        senario.text("マイクロソフト：1999年12月30日 時価総額が史上最高額を塗り替える");
+        _senario.text("マイクロソフト：1999年12月30日 時価総額が史上最高額を塗り替える");
         setTimeout(function(){
-            senario.text("");
+            _senario.text("");
             filterReset();
             setDisplayAll();
         }, HighlightTime);
@@ -202,9 +204,9 @@ function senarioDemo(){
     //2000
     setTimeout(function(){
         filterByCompany("CISCO");
-        senario.text("シスコ：時価総額5,000億US$に達し、マイクロソフトを抜き世界一");
+        _senario.text("シスコ：時価総額5,000億US$に達し、マイクロソフトを抜き世界一");
         setTimeout(function(){
-            senario.text("");
+            _senario.text("");
             filterReset();
             setDisplayAll();
         }, HighlightTime);
@@ -212,15 +214,15 @@ function senarioDemo(){
 
     //2001
     setTimeout(function(){
-        senario.text("ITバブル崩壊。その後、「選択と集中」の名のもと、電機業界の再編が加速してゆく");
+        _senario.text("ITバブル崩壊。その後、「選択と集中」の名のもと、電機業界の再編が加速してゆく");
     },SecPerYear*20);
 
     //2002
     setTimeout(function(){
         filterByCompany("HP");
-        senario.text("HP：コンピュータ大手コンパックを買収");
+        _senario.text("HP：コンピュータ大手コンパックを買収");
         setTimeout(function(){
-            senario.text("");
+            _senario.text("");
             filterReset();
             setDisplayAll();
         }, HighlightTime);
@@ -229,9 +231,9 @@ function senarioDemo(){
     //2003
     setTimeout(function(){
         filterByCompany("HITACHI");
-        senario.text("日立：IBM HDD部門を買収。三菱電機と半導体合弁ルネサステクノロジ設立");
+        _senario.text("日立：IBM HDD部門を買収。三菱電機と半導体合弁ルネサステクノロジ設立");
         setTimeout(function(){
-            senario.text("");
+            _senario.text("");
             filterReset();
             setDisplayAll();
         }, HighlightTime);
@@ -240,9 +242,9 @@ function senarioDemo(){
     //2004
     setTimeout(function(){
         filterByCompany("LENOVO");
-        senario.text("レノボ：IBM PC部門を買収");
+        _senario.text("レノボ：IBM PC部門を買収");
         setTimeout(function(){
-            senario.text("");
+            _senario.text("");
             filterReset();
             setDisplayAll();
         }, HighlightTime);
@@ -251,9 +253,9 @@ function senarioDemo(){
     //2005
     setTimeout(function(){
         filterByCompany("SIEMENS");
-        senario.text("シーメンス：携帯部門を台湾BenQへ売却");
+        _senario.text("シーメンス：携帯部門を台湾BenQへ売却");
         setTimeout(function(){
-            senario.text("");
+            _senario.text("");
             filterReset();
             setDisplayAll();
         }, HighlightTime);
@@ -262,9 +264,9 @@ function senarioDemo(){
     //2006
     setTimeout(function(){
         filterByCompany("TOSHIBA");
-        senario.text("東芝：米ウェスティングハウス買収。原子力発電世界三大メーカーの一角へ");
+        _senario.text("東芝：米ウェスティングハウス買収。原子力発電世界三大メーカーの一角へ");
         setTimeout(function(){
-            senario.text("");
+            _senario.text("");
             filterReset();
             setDisplayAll();
         }, HighlightTime);
@@ -273,9 +275,9 @@ function senarioDemo(){
     //2007
     setTimeout(function(){
         filterByCompany("APPLE");
-        senario.text("アップル：iPhone 発表");
+        _senario.text("アップル：iPhone 発表");
         setTimeout(function(){
-            senario.text("");
+            _senario.text("");
             filterReset();
             setDisplayAll();
         }, HighlightTime);
@@ -283,7 +285,7 @@ function senarioDemo(){
 
     //2008
     setTimeout(function(){
-        senario.text("リーマンショックによる世界金融危機。スマホ時代の幕開けとともに、多くの国内メーカーの携帯事業が失速");
+        _senario.text("リーマンショックによる世界金融危機。スマホ時代の幕開けとともに、多くの国内メーカーの携帯事業が失速");
     },SecPerYear*27);
 
     //2009
@@ -292,9 +294,9 @@ function senarioDemo(){
         setDisplayAll();
 
         filterByCompany("SANYO");
-        senario.text("三洋電機：パナソニックによる完全子会社化が決定");
+        _senario.text("三洋電機：パナソニックによる完全子会社化が決定");
         setTimeout(function(){
-            senario.text("");
+            _senario.text("");
             filterReset();
             setDisplayAll();
         }, HighlightTime);
@@ -303,9 +305,9 @@ function senarioDemo(){
     //2010
     setTimeout(function(){
         filterByCompany("ORACLE");
-        senario.text("オラクル：Javaやワークステーションで知られる米IT大手サン・マイクロシステムズ買収");
+        _senario.text("オラクル：Javaやワークステーションで知られる米IT大手サン・マイクロシステムズ買収");
         setTimeout(function(){
-            senario.text("");
+            _senario.text("");
             filterReset();
             setDisplayAll();
         }, HighlightTime);
@@ -314,9 +316,9 @@ function senarioDemo(){
     //2011
     setTimeout(function(){
         filterByCompany("FUJITSU");
-        senario.text("富士通：理研と共同でスーパーコンピュータ「京」を開発。世界一を奪還");
+        _senario.text("富士通：理研と共同でスーパーコンピュータ「京」を開発。世界一を奪還");
         setTimeout(function(){
-            senario.text("");
+            _senario.text("");
             filterReset();
             setDisplayAll();
         }, HighlightTime);
@@ -326,9 +328,9 @@ function senarioDemo(){
     setTimeout(function(){
         filterByCompany("APPLE");
         filterByCompany("SAMSUNG");
-        senario.text("アップルは時価総額世界1位に。サムスンとアップルの特許訴訟は泥仕合の様相へ");
+        _senario.text("アップルは時価総額世界1位に。サムスンとアップルの特許訴訟は泥仕合の様相へ");
         setTimeout(function(){
-            senario.text("");
+            _senario.text("");
             filterReset();
             setDisplayAll();
         }, HighlightTime*1.5);
@@ -337,9 +339,9 @@ function senarioDemo(){
     //2013
     setTimeout(function(){
         filterByCompany("DELL");
-        senario.text("デル：上場廃止。非公開株化");
+        _senario.text("デル：上場廃止。非公開株化");
         setTimeout(function(){
-            senario.text("");
+            _senario.text("");
             filterReset();
             setDisplayAll();
         }, HighlightTime);
@@ -349,9 +351,9 @@ function senarioDemo(){
     setTimeout(function(){
         filterByCompany("LENOVO");
         filterByCompany("NOKIA");
-        senario.text("ノキア：携帯部門をマイクロソフトへ売却　／　レノボ：Googleから携帯部門モトローラ・モビリティを買収");
+        _senario.text("ノキア：携帯部門をマイクロソフトへ売却　／　レノボ：Googleから携帯部門モトローラ・モビリティを買収");
         setTimeout(function(){
-            senario.text("");
+            _senario.text("");
             filterReset();
             setDisplayAll();
         }, HighlightTime*1.5);
@@ -360,9 +362,9 @@ function senarioDemo(){
     //2015
     setTimeout(function(){
         filterByCompany("TOSHIBA");
-        senario.text("東芝：不適切会計問題発覚");
+        _senario.text("東芝：不適切会計問題発覚");
         setTimeout(function(){
-            senario.text("");
+            _senario.text("");
             filterReset();
             setDisplayAll();
         }, HighlightTime);
